82a0db9c7d5fd9131edad232e1aa0b26afc01923
HaruChun <ikuharurudy@gmail.com>
2017-08-30 21:15:44 +0900

    models.py

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from home.models import Book
+admin.site.register(Book)
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+#from jsonfield import JSONField
 
+class Book(models.Model):
+	isbn = models.BigIntegerField(primary_key=True)
+	title = models.CharField(max_length=128)
+
+	def __str__(self):
+		return self.title
 # Create your models here.
